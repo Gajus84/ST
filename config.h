964d4cb8f131cd97d861e465d4bd444f6a3201ef
Gajus84 <44810947+Gajus84@users.noreply.github.com>
2021-06-20 15:34:46 +0200

    Add Altscreen Mouse Scrollback Patch

--- config.h
+++ config.h
@@ -177,8 +177,8 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ XK_ANY_MOD,            Button4, kscrollup,      {.i = 1} },
-	{ XK_ANY_MOD,            Button5, kscrolldown,    {.i = 1} },
+	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1},      0, /* !alt */ -1 },
+	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1},      0, /* !alt */ -1 },
 	{ ShiftMask,           Button4, ttysend,        {.s = "\031"} },
 	{ ShiftMask,           Button5, ttysend,        {.s = "\005"} },
 };
